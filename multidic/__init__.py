d08051ef723b7fb15aa70bcd241fb0a36fd07b96
LeeBDa <66620096+lbd-hfut@users.noreply.github.com>
2026-07-13 12:57:51 +0800

    Fix native COLMAP wheel dependency targets

--- multidic/__init__.py
+++ multidic/__init__.py
@@ -1,3 +1,3 @@
 """Multi-DIC Python workflow package."""
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
--- pymultidic/__init__.py
+++ pymultidic/__init__.py
@@ -17,7 +17,7 @@ from .api import (
     validate_project,
 )
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 __all__ = [
     "MDICConfig",
